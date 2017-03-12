3ba0102ceb27be15b9ac609144754fe08b65d11f
zhenglianfu <15067134439@163.com>
2017-03-12 21:43:20 +0800

    update

--- bin/cli.js
+++ bin/cli.js
@@ -4,5 +4,32 @@
  * Created by zhenglianfu on 2017/3/12.
  */
 var path = require('path');
-console.log(__dirname);
-console.log(process.argv);
+var fs = require('fs');
+var sourceDir = path.resolve(__dirname, '../src');
+var cmdArgs = process.argv.slice(2);
+if (cmdArgs.length == 0) {
+    console.log('用法：fuzl-cli 文件夹名');
+} else {
+    var targetDir = path.resolve(cmdArgs[0]);
+    if (isEmptyDir(targetDir)) {
+        copy(sourceDir, targetDir);
+    } else {
+        console.log(path.resolve(targetDir) + ' 不是空文件夹');
+    }
+}
+function isEmptyDir(dirPath){
+    if (fs.existsSync(dirPath)) {
+        var stat = fs.statSync(dirPath);
+        if (stat.isFile()) {
+            console.log(dirPath + '不是文件夹');
+            throw new TypeError(dirPath + '不是文件夹');
+            return false;
+        } else {
+            return fs.readdirSync(dirPath).length == 0;
+        }
+    }
+    return true;
+}
+function copy(src, target){
+
+}
